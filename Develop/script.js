d5d685184287fd67f55e8f900bd4667552615dbd
Erin Seppala <aseppala98@gmail.com>
2021-02-04 10:24:56 -0500

    added vars to script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
+var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numberChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "="];
+
+var minLength;
+var maxLength;
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
