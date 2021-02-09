21b2747c4b75aed27b2f1ff4316067d5136170df
Erin Seppala <aseppala98@gmail.com>
2021-02-08 22:42:46 -0500

    Cleaned up syntax in Develop/script.js, changed prompt of password length to a set variable instead of a random length chosen with two variables by the user.

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,5 @@
 // Assignment Code
+// Variables
 var generateBtn = document.querySelector("#generate");
 
 var upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
@@ -6,13 +7,15 @@ var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "="];
 
-var minLength;
-var maxLength;
-
 var confirmUpperChar;
 var confirmLowerChar;
 var confirmNumber;
 var confirmSpecialChar;
+
+var x;
+var y;
+var z;
+var passLength;
 
 // Write password to the #password input
 function writePassword() {
@@ -20,7 +23,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 function generatePassword(){
@@ -28,65 +30,56 @@ function generatePassword(){
     var tempPass = [];
     var lastPass = "";
     
-    var x;
-    var y;
-    var z;
-
-    minLength = parseInt(prompt("Please enter a minimum password length."));
-    while ((!(minLength >= 8)) || (!(minLength <= 128))){
-      minLength = parseInt(prompt("Invalid input. Please enter a length between 8 and 128."));
+// Prompt password length between 8-128 characters
+    passLength = parseInt(prompt("Please enter a password length between 8 and 128."));
+    while ((!(passLength >= 8)) || (!(passLength <= 128))){
+      passLength = parseInt(prompt("Invalid input. Please enter a length between 8 and 128."));
     }
 
-    maxLength = parseInt(prompt("Please enter a maximum password length."));
-    while ((!(maxLength >= minLength)) || (!(maxLength <= 128))){
-      maxLength = parseInt(prompt("Invalid input. Please enter a length between " + minLength + " and 128."));
-    }
-
+// Prompt if user wants uppercase characters generated in password
+  do{
     confirmUpperChar = confirm("Do you want uppercase characters?");
+    if (confirmUpperChar){
+      for(var i=0; i<upperChar.length; i++){
+        passChar.push(upperChar[i]);
+      }
+      tempPass.push(upperChar[Math.floor(Math.random()*upperChar.length)])
+    }
+
+// Prompt if user wants lowercase characters generated in password
     confirmLowerChar = confirm("Do you want lowercase characters?");
+    if (confirmLowerChar){
+      for(var j=0; j<lowerChar.length; j++){
+        passChar.push(lowerChar[j]);
+      }
+      tempPass.push(lowerChar[Math.floor(Math.random()*lowerChar.length)])
+    }
+
+// Prompt if user wants numbers generated in password
     confirmNumber = confirm("Do you want numbers?");
+    if (confirmNumber){  
+      for(var k=0; k<number.length; k++){
+        passChar.push(number[k]);
+      }
+      tempPass.push(number[Math.floor(Math.random()*number.length)])
+    }
+
+// Prompt if user wants special characters generated in password
     confirmSpecialChar = confirm("Do you want special characters?");
+    if (confirmSpecialChar){
+      for(var l=0; l<specialChar.length; l++){
+        passChar.push(specialChar[l]);
+      }
+      tempPass.push(specialChar[Math.floor(Math.random()*specialChar.length)])
+    }
 
-
+// if user didn't chose any character types, alert them that at least one character type must be used
     if (confirmUpperChar != true && confirmLowerChar != true && confirmNumber != true && confirmSpecialChar != true){
       alert("You must select at least one character type!");
-
-
-    }
-
-
-    var fixLength = maxLength - minLength + 1;
-  
-    var passLength = Math.floor((Math.random()*fixLength)+minLength);
-    
-  if (confirmUpperChar = true){
-    for(var j=0; j<upperChar.length; j++){
-      passChar.push(upperChar[j]);
-    }
-  }
-  
-  if (confirmLowerChar = true){
-    for(var i=0; i<lowerChar.length; i++){
-      passChar.push(lowerChar[i]);
     }
   }
 
-  if (confirmNumber = true){  
-    for(var k=0; k<number.length; k++){
-      passChar.push(number[k]);
-    }
-  }
-
-  if (confirmSpecialChar = true){
-    for(var l=0; l<specialChar.length; l++){
-      passChar.push(specialChar[l]);
-    }
-  }
-
-    tempPass.push(lowerChar[Math.floor(Math.random()*lowerChar.length)])
-    tempPass.push(upperChar[Math.floor(Math.random()*upperChar.length)])
-    tempPass.push(number[Math.floor(Math.random()*number.length)])
-    tempPass.push(specialChar[Math.floor(Math.random()*specialChar.length)])
+  while(!confirmLowerChar && !confirmUpperChar && !confirmNumber && !confirmSpecialChar);
 
     for(var m=tempPass.length; m<passLength; m++){
     tempPass.push(passChar[Math.floor(Math.random()*passChar.length)]);
